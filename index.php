81ca9031d7dbedc617b4fafcabaf4b7d07d79ceb
Amitdag12 <66032598+Amitdag12@users.noreply.github.com>
2021-08-23 21:19:50 +0300

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,15 @@
 <html>
 
 <head>
-
+ <style>
+        .dot {
+            height: 25px;
+            width: 25px;
+            background-color: red;
+            border-radius: 50%;
+            display: inline-block;
+        }
+    </style>
 </head>
 
 <body>
@@ -15,22 +23,33 @@
     <audio src="https://psyc-timer.herokuapp.com/double-beep.mp3" autostart="false" width="0" height="0" id="sound2" enablejavascript="true">
 
 </body>
-<script>
+<script type="text/javascript">
+   window.onload= function(){
+       var testAmount=window.prompt("כמה שאלונים יש");
+       var btn=document.getElementById("startBTN");
+       for(var i=0;i<testAmount;i++){
+var span =document.createElement("span");
+span.id="span"+i;
+span.classList.add("dot");
+document.body.append(span);
+       }
+   }
     var interval = 1000; // ms
     var expected = Date.now() + interval;
     var seconds = 0;
     var timer;
     var stop = false;
-
+    var testcount=0;
     function startTimer() {
+        stop = false;
         timer = setTimeout(step, interval);
         seconds = 0;
         expected = Date.now() + interval;
     }
 
-    function startTimer() {
-        stop = false;
-        timer = setTimeout(step, interval);
+    function stopTimer() {
+        stop = true;
+        clearTimeout(timer);
         seconds = 0;
         expected = Date.now() + interval;
     }
@@ -53,10 +72,13 @@
         }
         seconds++;
         document.getElementById("timer").textContent = ReturnTime(); // do what is to be done
-        if (seconds % 900 == 0) {
+        if (seconds % 900 == 0&&(seconds/60)%60!=00) {
             PlayBeep();
+            document.getElementById("span"+testcount).style.backgroundColor="yellow";
         } else if (seconds % 1200 == 0) {
             PlayTwoBeep();
+            document.getElementById("span"+testcount).style.backgroundColor="green";
+            testcount++;
         }
         expected += interval;
         if (!stop) {
